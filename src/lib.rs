e2a570ca349b815a663a6cf186f11279fe8d9fa9
Walfie <walfington@gmail.com>
2019-09-08 11:58:26 -0400

    Move functions into library

---
+++ src/lib.rs
@@ -0,0 +1,154 @@
+use openssl::hash::MessageDigest;
+use openssl::pkey::{HasPrivate, HasPublic, PKeyRef};
+use openssl::sign::{Signer, Verifier};
+use std::error::Error;
+use std::fmt::Write as _;
+use std::io::Write as _;
+
+pub fn validate_signature_parts<'a, T>(
+    request: &http::Request<T>,
+    parts: &SignatureParts<'a>,
+    digest: MessageDigest,
+    public_key: &PKeyRef<impl HasPublic>,
+) -> Result<bool, Box<dyn Error>> {
+    let signature = base64::decode(parts.signature)?;
+
+    let mut verifier = Verifier::new(digest, public_key)?;
+    let mut to_verify: Vec<u8> = Vec::new();
+
+    for header_name in parts.headers.unwrap_or("date").split(' ') {
+        if header_name == "(request-target)" {
+            write!(
+                &mut to_verify,
+                "(request-target): {} {}\n",
+                request.method().as_str().to_ascii_lowercase(),
+                request.uri()
+            )?;
+        } else if let Some(header_value) = request.headers().get(header_name) {
+            write!(
+                &mut to_verify,
+                "{}: {}\n",
+                header_name,
+                header_value.to_str()?
+            )?;
+        } else {
+            return Ok(false);
+        }
+    }
+
+    // `pop` to remove the trailing newline. If it returns `None`, there were no headers, so we
+    // should default to checking the `date` header.
+    if to_verify.pop().is_none() {
+        if let Some(date) = request.headers().get("date") {
+            write!(&mut to_verify, "date: {}", date.to_str()?)?;
+        } else {
+            return Ok(false);
+        }
+    }
+
+    verifier.update(&to_verify)?;
+
+    Ok(verifier.verify(&signature)?)
+}
+
+#[derive(Debug)]
+pub struct SignatureParts<'a> {
+    pub headers: Option<&'a str>,
+    pub key_id: &'a str,
+    pub signature: &'a str,
+    pub algorithm: Option<&'a str>,
+}
+
+pub fn parse_signature_parts<'a>(signature_string: &'a str) -> Option<SignatureParts<'a>> {
+    let mut headers = None;
+    let mut key_id = None;
+    let mut algorithm = None;
+    let mut signature = None;
+
+    for part in signature_string.split(',') {
+        let mut kv = part.splitn(2, '=');
+
+        if let (Some(key), Some(value)) = (kv.next(), kv.next()) {
+            if !(value.starts_with('"') && value.ends_with('"')) {
+                return None;
+            }
+
+            let value = value.trim_start_matches('"').trim_end_matches('"');
+
+            match key {
+                "headers" => headers = Some(value),
+                "keyId" => key_id = Some(value),
+                "algorithm" => algorithm = Some(value),
+                "signature" => signature = Some(value),
+                _ => {}
+            }
+        } else {
+            return None;
+        }
+    }
+
+    if let (Some(k), Some(s)) = (key_id, signature) {
+        return Some(SignatureParts {
+            key_id: k,
+            signature: s,
+            headers,
+            algorithm,
+        });
+    } else {
+        return None;
+    }
+}
+
+pub fn create_signature_header<T>(
+    request: &http::Request<T>,
+    key_id: &str,
+    digest: MessageDigest,
+    private_key: &PKeyRef<impl HasPrivate>,
+) -> Result<String, Box<dyn Error>> {
+    let signature = compute_signature(&request, digest, &private_key)?;
+    let base64_signature = base64::encode(&signature);
+
+    let mut output = String::new();
+
+    write!(
+        &mut output,
+        "keyId=\"{}\",headers=\"(request-target)",
+        key_id
+    )?;
+    for (header_name, _) in request.headers() {
+        write!(&mut output, " {}", header_name.as_str())?;
+    }
+
+    write!(&mut output, "\",signature=\"{}\"", base64_signature)?;
+
+    Ok(output)
+}
+
+pub fn compute_signature<T>(
+    request: &http::Request<T>,
+    digest: MessageDigest,
+    private_key: &PKeyRef<impl HasPrivate>,
+) -> Result<Vec<u8>, Box<dyn Error>> {
+    let mut signer = Signer::new(digest, private_key)?;
+
+    let mut payload_to_sign: Vec<u8> = Vec::new();
+    write!(
+        &mut payload_to_sign,
+        "(request-target): {} {}",
+        request.method().as_str().to_ascii_lowercase(),
+        request.uri()
+    )?;
+
+    for (header_name, header_value) in request.headers() {
+        // HeaderName's `as_str` is guaranteed to be lowercase
+        write!(
+            &mut payload_to_sign,
+            "\n{}: {}",
+            header_name.as_str(),
+            header_value.to_str()?
+        )?;
+    }
+
+    signer.update(&payload_to_sign)?;
+    Ok(signer.sign_to_vec()?)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 use openssl::hash::MessageDigest;
-use openssl::pkey::{HasPrivate, HasPublic, PKey, PKeyRef};
-use openssl::sign::{Signer, Verifier};
+use openssl::pkey::PKey;
 use std::error::Error;
-use std::fmt::Write as _;
-use std::io::Write as _;
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Values taken from:
@@ -45,14 +42,14 @@ G6aFKaqQfOXKCyWoUiVknQJAXrlgySFci/2ueKlIE1QqIiLSZ8V8OlpFLRnb1pzI
     let public_key = PKey::public_key_from_pem(public_pem)?;
 
     let signature =
-        create_signature_header(&request, "Test", MessageDigest::sha256(), &private_key)?;
+        httpsig::create_signature_header(&request, "Test", MessageDigest::sha256(), &private_key)?;
 
     request
         .headers_mut()
         .insert("signature", signature.parse()?);
 
-    if let Some(parts) = parse_signature_parts(&signature) {
-        assert!(validate_signature_parts(
+    if let Some(parts) = httpsig::parse_signature_parts(&signature) {
+        assert!(httpsig::validate_signature_parts(
             &request,
             &parts,
             MessageDigest::sha256(),
@@ -63,154 +60,6 @@ G6aFKaqQfOXKCyWoUiVknQJAXrlgySFci/2ueKlIE1QqIiLSZ8V8OlpFLRnb1pzI
     }
 
     Ok(())
-}
-
-fn validate_signature_parts<'a, T>(
-    request: &http::Request<T>,
-    parts: &SignatureParts<'a>,
-    digest: MessageDigest,
-    public_key: &PKeyRef<impl HasPublic>,
-) -> Result<bool, Box<dyn Error>> {
-    let signature = base64::decode(parts.signature)?;
-
-    let mut verifier = Verifier::new(digest, public_key)?;
-    let mut to_verify: Vec<u8> = Vec::new();
-
-    for header_name in parts.headers.split(' ') {
-        if header_name == "(request-target)" {
-            write!(
-                &mut to_verify,
-                "(request-target): {} {}\n",
-                request.method().as_str().to_ascii_lowercase(),
-                request.uri()
-            )?;
-        } else if let Some(header_value) = request.headers().get(header_name) {
-            write!(
-                &mut to_verify,
-                "{}: {}\n",
-                header_name,
-                header_value.to_str()?
-            )?;
-        } else {
-            return Ok(false);
-        }
-    }
-
-    // `pop` to remove the trailing newline. If it returns `None`, there were no headers, so we
-    // should default to checking the `date` header.
-    if to_verify.pop().is_none() {
-        if let Some(date) = request.headers().get("date") {
-            write!(&mut to_verify, "date: {}", date.to_str()?)?;
-        } else {
-            return Ok(false);
-        }
-    }
-
-    verifier.update(&to_verify)?;
-
-    Ok(verifier.verify(&signature)?)
-}
-
-#[derive(Debug)]
-struct SignatureParts<'a> {
-    headers: &'a str,
-    key_id: &'a str,
-    signature: &'a str,
-    algorithm: Option<&'a str>,
-}
-
-fn parse_signature_parts<'a>(signature_string: &'a str) -> Option<SignatureParts<'a>> {
-    let mut headers = None;
-    let mut key_id = None;
-    let mut algorithm = None;
-    let mut signature = None;
-
-    for part in signature_string.split(',') {
-        let mut kv = part.splitn(2, '=');
-
-        if let (Some(key), Some(value)) = (kv.next(), kv.next()) {
-            if !(value.starts_with('"') && value.ends_with('"')) {
-                return None;
-            }
-
-            let value = value.trim_start_matches('"').trim_end_matches('"');
-
-            match key {
-                "headers" => headers = Some(value),
-                "keyId" => key_id = Some(value),
-                "algorithm" => algorithm = Some(value),
-                "signature" => signature = Some(value),
-                _ => {}
-            }
-        } else {
-            return None;
-        }
-    }
-
-    if let (Some(k), Some(s)) = (key_id, signature) {
-        return Some(SignatureParts {
-            headers: headers.unwrap_or("date"),
-            key_id: k,
-            signature: s,
-            algorithm,
-        });
-    } else {
-        return None;
-    }
-}
-
-fn create_signature_header<T>(
-    request: &http::Request<T>,
-    key_id: &str,
-    digest: MessageDigest,
-    private_key: &PKeyRef<impl HasPrivate>,
-) -> Result<String, Box<dyn Error>> {
-    let signature = compute_signature(&request, digest, &private_key)?;
-    let base64_signature = base64::encode(&signature);
-
-    let mut output = String::new();
-
-    write!(
-        &mut output,
-        "keyId=\"{}\",headers=\"(request-target)",
-        key_id
-    )?;
-    for (header_name, _) in request.headers() {
-        write!(&mut output, " {}", header_name.as_str())?;
-    }
-
-    write!(&mut output, "\",signature=\"{}\"", base64_signature)?;
-
-    Ok(output)
-}
-
-fn compute_signature<T>(
-    request: &http::Request<T>,
-    digest: MessageDigest,
-    private_key: &PKeyRef<impl HasPrivate>,
-) -> Result<Vec<u8>, Box<dyn Error>> {
-    let mut signer = Signer::new(digest, private_key)?;
-
-    let mut payload_to_sign: Vec<u8> = Vec::new();
-    write!(
-        &mut payload_to_sign,
-        "(request-target): {} {}",
-        request.method().as_str().to_ascii_lowercase(),
-        request.uri()
-    )?;
-
-    for (header_name, header_value) in request.headers() {
-        // HeaderName's `as_str` is guaranteed to be lowercase
-        write!(
-            &mut payload_to_sign,
-            "\n{}: {}",
-            header_name.as_str(),
-            header_value.to_str()?
-        )?;
-    }
-
-    signer.update(&payload_to_sign)?;
-    Ok(signer.sign_to_vec()?)
 }
 
 // Not returning a `Result` here because it's out of scope for the library
